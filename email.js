2ce5520015b41b83aa160c3848035938b791fa5f
Jonáš Holub <h.kolombooo@gmail.com>
2024-05-28 15:22:29 +0200

    first commit

---
+++ email.js
@@ -0,0 +1,57 @@
+function count(needle, haystack) { // Funkce na součet znaku ve stringu (N: znak, H: string)
+    for(var count=-1,index=-2; index != -1; count++,index=haystack.indexOf(needle,index+1) );
+    return count;
+}
+
+function validate(email) {
+    while (true) {
+        if (count("@", email) !== 1) {
+            return false;
+        }
+
+        var name = email.split("@")[0]; // Sekce stringu před zavináčem
+        var dom = email.split("@")[1]; // Sekce stringu za zavináčem
+        var tld = dom.split(".")[0]; // Sekce domény (dom) za tečkou
+
+        if (count(".", dom) <= 0) {
+            return false;
+        }
+
+        if (dom.length < 3) {
+            return false;
+        }
+
+        if (name.length < 1) {
+            return false;
+        }
+
+        if (tld.length < 1) {
+            return false;
+        }
+
+        if (dom.split("")[dom.length - 1] === "." || dom.split("")[0] === ".") {
+            return false;
+        }
+
+        return true; // Email prošel všechny kontroly, je validní
+        break; // Ukončit cyklus
+    }
+}
+
+const email = "jonas@gmail.com"; // Počáteční hodnota
+console.log(validate(email));
+
+// ALTERNATIVNÍ KÓD
+// Hromadná validace
+// Pro zapnutí změnit podmínku
+
+if (true) {
+    console.log("--------------------");
+    const emails = ["jonas.4holubgmail.com", "jonas.holub@mensagymnzium.cz"];
+    console.log("Hromadné ověření " + emails.length + " adres.");
+    console.log("+-----+----------------------------------------------");
+    for (i = 0; i < emails.length; i++) {
+        console.log("|" + validate(emails[i]) + " |" + emails[i]);
+    }
+    console.log("+-----+----------------------------------------------");
+}
